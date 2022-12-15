81f04319e8da398cf6302b6fa7e9e11a799d64c2
Shoyemi20 <shoyemiesther20@gmail.com>
2022-12-15 21:53:39 +0100

    first commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,43 @@
+import React, { useState} from 'react';
+import logo from '../images/logo.png';
+import {Link} from 'react-scroll'
+
+
+function Navbar() {
+
+  const [nav, setnav] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 50) {
+      setnav(true)
+    } 
+    else {
+    setnav(false);
+    }
+}
+
+window.addEventListener('scroll', changeBackground);
+
+  return (
+    
+  <nav className={nav ? "nav active" :"nav"}>
+    <Link to='#' className='logo'>
+      <img src={logo} alt='' />
+    </Link>
+    <input className='menu-btn' type='checkbox' id='menu-btn' />
+    <label ClassName='menu-icon' for='menu-btn'>
+      <span ClassName='nav-icon'></span>
+    </label>
+    <ul ClassName='menu'>
+      <li><Link to='#'>Header</Link></li>
+      <li><Link to='#'>Features</Link></li>
+      <li><Link to='#'>Offer</Link></li>
+      <li><Link to='#'>About</Link></li>
+      <li><Link to='#'>Contact</Link></li>
+    </ul>
+  </nav>
+  )
+}
+
+
+export default Navbar;
