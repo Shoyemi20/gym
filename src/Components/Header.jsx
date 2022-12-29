dcc6d4e7fdd53231d38ae3dd5b7c95ba5be2b372
Shoyemi20 <shoyemiesther20@gmail.com>
2022-12-29 09:00:20 +0100

    header & feature

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,16 +3,16 @@ import React from "react";
 function Header() {
   return (
     <div id='main'>
-      <div className="header-heading">
-        <h2>STEP UP YOUR</h2>
-        <h1><span>FITNESS</span>WITH US</h1>
-        <p className='details'>Build Your Body And Fitness With Professional Touch</p>
-        <div className='header-btns'>   // eslint-disable-next-line
+      <div className='header-heading'>
+        <h1>STEP UP YOUR</h1>
+        <h2><span>FITNESS</span>WITH US</h2>
+        <p className='details'>Build Your Body And Fitness With Professtional Touch</p>
+        <div className='header-btns'>
           <a href='#' className='header-btn'>JOIN US</a>
         </div>
-       </div>
-    </div>
-  )
-}
+        </div>
+      </div>
+      )
+      }
 
 export default Header;
